b91a405816719b5acba123ecef51e81c57295dea
cebarobot <ceba_robot@outlook.com>
2021-05-12 13:03:56 -0700

    09-router: stage commit

--- 09-router/ip.c
+++ 09-router/ip.c
@@ -1,4 +1,5 @@
 #include "ip.h"
+#include "arp.h"
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -10,5 +11,30 @@
 // packet.
 void handle_ip_packet(iface_info_t *iface, char *packet, int len)
 {
+	// TODO:
 	fprintf(stderr, "TODO: handle ip packet.\n");
+
+	// TODO: parse ip packet header
+
+	// TODO: handle TTL
+	
+	// TODO: if TTL <= 0:
+	if (0) {
+		// TODO: drop packet & send ICMP info.
+		return;
+	}
+
+	// TODO: search in route table
+
+	// TODO: if found:
+	// TODO check ICMP echo request (ping).
+	if (0) {
+		// TODO: send ICMP reply.
+		return;
+	}
+	// TODO: forward (IP) packet through arpcache lookup 
+	// iface_send_packet_by_arp(xxx, xxxxxx, packet, len);
+
+	// TODO: if not found: 
+	// TODO: send ICMP info.
 }
