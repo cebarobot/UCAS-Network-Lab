bffb325ab866a4dbd59cf76d32f219a71f79700a
cebarobot <ceba_robot@outlook.com>
2021-06-30 12:13:50 -0700

    16-tcp_stack: init

---
+++ 16-tcp_stack/tcp_apps.c
@@ -0,0 +1,221 @@
+#include "tcp_sock.h"
+
+#include "log.h"
+
+#include <stdlib.h>
+#include <unistd.h>
+
+char filename[100];
+
+// tcp server application, listens to port (specified by arg) and serves only one
+// connection request
+void *tcp_server(void *arg)
+{
+	u16 port = *(u16 *)arg;
+	struct tcp_sock *tsk = alloc_tcp_sock();
+
+	struct sock_addr addr;
+	addr.ip = htonl(0);
+	addr.port = port;
+	if (tcp_sock_bind(tsk, &addr) < 0) {
+		log(ERROR, "tcp_sock bind to port %hu failed", ntohs(port));
+		exit(1);
+	}
+
+	if (tcp_sock_listen(tsk, 3) < 0) {
+		log(ERROR, "tcp_sock listen failed");
+		exit(1);
+	}
+
+	log(DEBUG, "listen to port %hu.", ntohs(port));
+
+	struct tcp_sock *csk = tcp_sock_accept(tsk);
+
+	log(DEBUG, "accept a connection.");
+
+	char rbuf[1001];
+	char wbuf[1024];
+	int rlen = 0;
+	while (1) {
+		rlen = tcp_sock_read(csk, rbuf, 1000);
+		if (rlen == 0) {
+			log(DEBUG, "tcp_sock_read return 0, finish transmission.");
+			break;
+		} 
+		else if (rlen > 0) {
+			rbuf[rlen] = '\0';
+			sprintf(wbuf, "server echoes: %s", rbuf);
+			if (tcp_sock_write(csk, wbuf, strlen(wbuf)) < 0) {
+				log(DEBUG, "tcp_sock_write return negative value, something goes wrong.");
+				exit(1);
+			}
+		}
+		else {
+			log(DEBUG, "tcp_sock_read return negative value, something goes wrong.");
+			exit(1);
+		}
+	}
+
+	log(DEBUG, "close this connection.");
+
+	tcp_sock_close(csk);
+	
+	tcp_sock_close(tsk);
+	
+	return NULL;
+}
+
+// tcp client application, connects to server (ip:port specified by arg), each
+// time sends one bulk of data and receives one bulk of data 
+void *tcp_client(void *arg)
+{
+	struct sock_addr *skaddr = arg;
+
+	struct tcp_sock *tsk = alloc_tcp_sock();
+
+	if (tcp_sock_connect(tsk, skaddr) < 0) {
+		log(ERROR, "tcp_sock connect to server ("IP_FMT":%hu)failed.", \
+				NET_IP_FMT_STR(skaddr->ip), ntohs(skaddr->port));
+		exit(1);
+	}
+
+	log(DEBUG, "connect success.");
+
+	char *data = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	int dlen = strlen(data);
+	char *wbuf = malloc(dlen+1);
+	char rbuf[1001];
+	int rlen = 0;
+
+	int n = 10;
+	for (int i = 0; i < n; i++) {
+		memcpy(wbuf, data+i, dlen-i);
+		if (i > 0) memcpy(wbuf+(dlen-i), data, i);
+
+		if (tcp_sock_write(tsk, wbuf, dlen) < 0)
+			break;
+
+		log(DEBUG, "send packet");
+
+		rlen = tcp_sock_read(tsk, rbuf, 1000);
+		if (rlen == 0) {
+			log(DEBUG, "tcp_sock_read return 0, finish transmission.");
+			break;
+		}
+		else if (rlen > 0) {
+			rbuf[rlen] = '\0';
+			fprintf(stdout, "%s\n", rbuf);
+		}
+		else {
+			log(DEBUG, "tcp_sock_read return negative value, something goes wrong.");
+			exit(1);
+		}
+		sleep(1);
+	}
+
+	tcp_sock_close(tsk);
+
+	free(wbuf);
+
+	return NULL;
+}
+
+
+// tcp server application, listens to port (specified by arg) and serves only one
+// connection request
+void *tcp_server_file(void *arg)
+{
+	FILE * f = fopen(filename, "wb");
+	if (!f) {
+		log(ERROR, "cannot open file");
+	}
+	log(DEBUG, "open file %s", filename);
+
+	u16 port = *(u16 *)arg;
+	struct tcp_sock *tsk = alloc_tcp_sock();
+
+	struct sock_addr addr;
+	addr.ip = htonl(0);
+	addr.port = port;
+	if (tcp_sock_bind(tsk, &addr) < 0) {
+		log(ERROR, "tcp_sock bind to port %hu failed", ntohs(port));
+		exit(1);
+	}
+
+	if (tcp_sock_listen(tsk, 3) < 0) {
+		log(ERROR, "tcp_sock listen failed");
+		exit(1);
+	}
+
+	log(DEBUG, "listen to port %hu.", ntohs(port));
+
+	struct tcp_sock *csk = tcp_sock_accept(tsk);
+
+	log(DEBUG, "accept a connection.");
+
+	char data_buf[1030];
+	int data_len = 0;
+	while (1) {
+		data_len = tcp_sock_read(csk, data_buf, 1024);
+		if (data_len > 0) {
+			fwrite(data_buf, 1, data_len, f);
+		} else {
+			log(DEBUG, "peer closed.");
+			break;
+		}
+	}
+
+	fclose(f);
+	log(DEBUG, "close this connection.");
+
+	tcp_sock_close(csk);
+	
+	tcp_sock_close(tsk);
+	
+	return NULL;
+}
+
+// tcp client application, connects to server (ip:port specified by arg), each
+// time sends one bulk of data and receives one bulk of data 
+void *tcp_client_file(void *arg)
+{
+	FILE * f = fopen(filename, "rb");
+	if (!f) {
+		log(ERROR, "cannot open file");
+	}
+	log(DEBUG, "open file %s", filename);
+
+	struct sock_addr *skaddr = arg;
+
+	struct tcp_sock *tsk = alloc_tcp_sock();
+
+	if (tcp_sock_connect(tsk, skaddr) < 0) {
+		log(ERROR, "tcp_sock connect to server ("IP_FMT":%hu)failed.", \
+				NET_IP_FMT_STR(skaddr->ip), ntohs(skaddr->port));
+		exit(1);
+	}
+
+	log(DEBUG, "connect success.");
+
+	char data_buf[1030];
+	int data_len = 0;
+
+	int send_size = 0;
+	while (1) {
+		data_len = fread(data_buf, 1, 1024, f);
+		if (data_len > 0) {
+			send_size += data_len;
+			printf("sent %d Bytes\n", send_size);
+			tcp_sock_write(tsk, data_buf, data_len);
+		} else {
+			log(DEBUG, "the file is end.");
+			break;
+		}
+		usleep(10000);
+	}
+
+	fclose(f);
+	tcp_sock_close(tsk);
+
+	return NULL;
+}
---
+++ 16-tcp_stack/tcp_timer.c
@@ -0,0 +1,83 @@
+#include "tcp.h"
+#include "tcp_timer.h"
+#include "tcp_sock.h"
+#include "log.h"
+
+#include <stdio.h>
+#include <unistd.h>
+#include <pthread.h>
+
+static struct list_head timer_list;
+static pthread_mutex_t timer_list_lock = PTHREAD_MUTEX_INITIALIZER;
+
+// scan the timer_list, find the tcp sock which stays for at 2*MSL, release it
+void tcp_scan_timer_list()
+{
+	// fprintf(stdout, "implement %s please.\n", __FUNCTION__);
+
+	pthread_mutex_lock(&timer_list_lock);
+
+	struct tcp_timer * timer_p = NULL, * timer_q = NULL;
+	list_for_each_entry_safe(timer_p, timer_q, &timer_list, list) {
+		if (timer_p->enable) {
+			timer_p->timeout -= TCP_TIMER_SCAN_INTERVAL;
+			if (timer_p->timeout <= 0) {
+				struct tcp_sock * tsk = NULL;
+				if (timer_p->type == TIMER_TYPE_TIME_WAIT) {
+					// do TCP_TIME_WAIT to TCP_CLOSED
+					timer_p->enable = 0;
+
+					tsk = timewait_to_tcp_sock(timer_p);
+					assert(tsk->state == TCP_TIME_WAIT);
+					tcp_set_state(tsk, TCP_CLOSED);
+
+					tcp_unhash(tsk);
+					tcp_bind_unhash(tsk);
+					
+					// remove reference from timewait list
+					list_delete_entry(&timer_p->list);
+					free_tcp_sock(tsk);
+
+					// just leave the closed sock in accept_queue/user
+				} else if (timer_p->type == TIMER_TYPE_RETRANS) {
+					tsk = retranstimer_to_tcp_sock(timer_p);
+					// nothing to do in this version
+				}
+			}
+		}
+	}
+
+	pthread_mutex_unlock(&timer_list_lock);
+}
+
+// set the timewait timer of a tcp sock, by adding the timer into timer_list
+void tcp_set_timewait_timer(struct tcp_sock *tsk)
+{
+	// fprintf(stdout, "implement %s please.\n", __FUNCTION__);
+
+	tsk->timewait.enable = 1;
+	tsk->timewait.type = TIMER_TYPE_TIME_WAIT;
+	tsk->timewait.timeout = TCP_TIMEWAIT_TIMEOUT;
+
+	// refer to this sock in timewait list
+	tsk->ref_cnt += 1;
+	log(DEBUG, "insert " IP_FMT ":%hu <-> " IP_FMT ":%hu to timewait, ref_cnt += 1", 
+			HOST_IP_FMT_STR(tsk->sk_sip), tsk->sk_sport,
+			HOST_IP_FMT_STR(tsk->sk_dip), tsk->sk_dport);
+
+	pthread_mutex_lock(&timer_list_lock);
+	list_add_tail(&tsk->timewait.list, &timer_list);
+	pthread_mutex_unlock(&timer_list_lock);
+}
+
+// scan the timer_list periodically by calling tcp_scan_timer_list
+void *tcp_timer_thread(void *arg)
+{
+	init_list_head(&timer_list);
+	while (1) {
+		usleep(TCP_TIMER_SCAN_INTERVAL);
+		tcp_scan_timer_list();
+	}
+
+	return NULL;
+}
